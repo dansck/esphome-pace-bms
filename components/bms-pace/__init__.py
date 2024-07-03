0745e3af3f7b8754ef2d8b0d27fec501b459f2f7
dansck <git@simecek.eu>
2024-07-03 11:42:57 +0200

    modify init__.py

--- components/bms-pace/__init__.py
+++ components/bms-pace/__init__.py
@@ -1,128 +1,165 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.components import sensor, uart
-from esphome.const import CONF_ID, CONF_SENSOR
+from esphome.const import CONF_ID
 
-# Vytvoření referencí pro naše vlastní komponenty
+DEPENDENCIES = ['uart']
+
 pace_bms_ns = cg.esphome_ns.namespace('pace_bms')
 PaceBMS = pace_bms_ns.class_('PaceBMS', cg.Component, uart.UARTDevice)
 
-# Definice všech senzorů, které bude naše komponenta podporovat
-SENSOR_KEYS = [
-    "voltage_sensor",
-    "current_sensor",
-    "remaining_capacity_sensor",
-    "nominal_capacity_sensor",
-    "full_capacity_sensor",
-    "cycles_sensor",
-    "state_of_health_sensor",
-    "state_of_charge_sensor",
-    "cell_max_volt_diff_sensor",
-    "charge_fet_sensor",
-    "discharge_fet_sensor",
-    "balancing1_sensor",
-    "balancing2_sensor",
-    "ac_in_sensor",
-    "current_limit_sensor",
-    "heart_sensor",
-    "pack_indicate_sensor",
-    "protection_discharge_current_sensor",
-    "protection_charge_current_sensor",
-    "protection_short_circuit_sensor",
-    "reverse_sensor",
-]
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(PaceBMS),
+        cv.Optional('voltage_sensor'): sensor.sensor_schema(),
+        cv.Optional('current_sensor'): sensor.sensor_schema(),
+        cv.Optional('remaining_capacity_sensor'): sensor.sensor_schema(),
+        cv.Optional('nominal_capacity_sensor'): sensor.sensor_schema(),
+        cv.Optional('full_capacity_sensor'): sensor.sensor_schema(),
+        cv.Optional('cycles_sensor'): sensor.sensor_schema(),
+        cv.Optional('state_of_health_sensor'): sensor.sensor_schema(),
+        cv.Optional('state_of_charge_sensor'): sensor.sensor_schema(),
+        cv.Optional('cell_max_volt_diff_sensor'): sensor.sensor_schema(),
+        cv.Optional('charge_fet_sensor'): sensor.sensor_schema(),
+        cv.Optional('discharge_fet_sensor'): sensor.sensor_schema(),
+        cv.Optional('balancing1_sensor'): sensor.sensor_schema(),
+        cv.Optional('balancing2_sensor'): sensor.sensor_schema(),
+        cv.Optional('ac_in_sensor'): sensor.sensor_schema(),
+        cv.Optional('current_limit_sensor'): sensor.sensor_schema(),
+        cv.Optional('heart_sensor'): sensor.sensor_schema(),
+        cv.Optional('pack_indicate_sensor'): sensor.sensor_schema(),
+        cv.Optional('protection_discharge_current_sensor'): sensor.sensor_schema(),
+        cv.Optional('protection_charge_current_sensor'): sensor.sensor_schema(),
+        cv.Optional('protection_short_circuit_sensor'): sensor.sensor_schema(),
+        cv.Optional('reverse_sensor'): sensor.sensor_schema(),
 
-CELL_VOLTAGE_KEYS = [f"cell_voltage_sensor_{i}" for i in range(1, 18)]
-TEMPERATURE_KEYS = [f"temperature_sensor_{i}" for i in range(1, 7)]
+        cv.Optional('cell_voltage_sensor_1'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_2'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_3'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_4'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_5'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_6'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_7'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_8'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_9'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_10'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_11'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_12'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_13'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_14'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_15'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_16'): sensor.sensor_schema(),
+        cv.Optional('cell_voltage_sensor_17'): sensor.sensor_schema(),
 
-# Kombinace všech senzorů
-ALL_SENSOR_KEYS = SENSOR_KEYS + CELL_VOLTAGE_KEYS + TEMPERATURE_KEYS
+        cv.Optional('temperature_sensor_1'): sensor.sensor_schema(),
+        cv.Optional('temperature_sensor_2'): sensor.sensor_schema(),
+        cv.Optional('temperature_sensor_3'): sensor.sensor_schema(),
+        cv.Optional('temperature_sensor_4'): sensor.sensor_schema(),
+        cv.Optional('temperature_sensor_5'): sensor.sensor_schema(),
+        cv.Optional('temperature_sensor_6'): sensor.sensor_schema(),
+    }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+)
 
-# Konfigurace pro YAML
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(PaceBMS),
-    cv.Optional(CONF_SENSOR, default={}): cv.Schema({
-        cv.Optional(key): sensor.sensor_schema() for key in ALL_SENSOR_KEYS
-    })
-}).extend(uart.UART_DEVICE_SCHEMA)
-
-# Funkce pro konfiguraci
 def to_code(config):
-    # Vytvoření instance naší komponenty
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(var.set_uart_parent(uart.UARTDevice(config[CONF_ID])))
-    
-    # Přidání všech senzorů z YAML do komponenty
-    for key in ALL_SENSOR_KEYS:
-        if key in config[CONF_SENSOR]:
-            sensor_conf = config[CONF_SENSOR][key]
-            sens = yield sensor.new_sensor(sensor_conf)
-            cg.add(getattr(var, f"set_{key}")(sens))
+    cg.add(var.set_timeout(5000))
 
-    # Zaregistrování komponenty
-    cg.add(var.register_component())
+    if 'voltage_sensor' in config:
+        sens = yield sensor.new_sensor(config['voltage_sensor'])
+        cg.add(var.set_voltage_sensor(sens))
 
-# Registrace konfigurace a inicializační funkce
-async def to_code(config):
-    # Inicializace UART
-    uart_var = await cg.get_variable(config[CONF_ID])
-    cg.add(uart_var.set_uart_parent(uart_var))
-    
-    # Přidání všech senzorů
-    for key in SENSOR_KEYS:
-        if key in config[CONF_SENSOR]:
-            sens = await sensor.new_sensor(config[CONF_SENSOR][key])
-            cg.add(getattr(uart_var, f"set_{key}")(sens))
-    
-    # Přidání senzorů pro napětí článků
-    for i, key in enumerate(CELL_VOLTAGE_KEYS):
-        if key in config[CONF_SENSOR]:
-            sens = await sensor.new_sensor(config[CONF_SENSOR][key])
-            cg.add(uart_var.set_cell_voltage_sensor(i, sens))
-    
-    # Přidání teplotních senzorů
-    for i, key in enumerate(TEMPERATURE_KEYS):
-        if key in config[CONF_SENSOR]:
-            sens = await sensor.new_sensor(config[CONF_SENSOR][key])
-            cg.add(uart_var.set_temperature_sensor(i, sens))
+    if 'current_sensor' in config:
+        sens = yield sensor.new_sensor(config['current_sensor'])
+        cg.add(var.set_current_sensor(sens))
 
-# Registrace komponenty v ESPHome
-async def to_code(config):
-    # Vytvoření instance naší komponenty
-    var = cg.new_Pvariable(config[CONF_ID])
-    
-    # Připojení UART komponenty
-    uart_component = await cg.get_variable(config[CONF_ID])
-    cg.add(var.set_uart_parent(uart_component))
+    if 'remaining_capacity_sensor' in config:
+        sens = yield sensor.new_sensor(config['remaining_capacity_sensor'])
+        cg.add(var.set_remaining_capacity_sensor(sens))
 
-    # Přidání všech senzorů z YAML do komponenty
-    for key in ALL_SENSOR_KEYS:
-        if key in config[CONF_SENSOR]:
-            sensor_conf = config[CONF_SENSOR][key]
-            sens = await sensor.new_sensor(sensor_conf)
-            cg.add(getattr(var, f"set_{key}")(sens))
+    if 'nominal_capacity_sensor' in config:
+        sens = yield sensor.new_sensor(config['nominal_capacity_sensor'])
+        cg.add(var.set_nominal_capacity_sensor(sens))
 
-    # Zaregistrování komponenty
-    cg.add(var.register_component())
+    if 'full_capacity_sensor' in config:
+        sens = yield sensor.new_sensor(config['full_capacity_sensor'])
+        cg.add(var.set_full_capacity_sensor(sens))
 
-# Registrace naší komponenty v ESPHome
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
+    if 'cycles_sensor' in config:
+        sens = yield sensor.new_sensor(config['cycles_sensor'])
+        cg.add(var.set_cycles_sensor(sens))
 
-    for key in SENSOR_KEYS:
-        if key in config[CONF_SENSOR]:
-            sens = await sensor.new_sensor(config[CONF_SENSOR][key])
-            cg.add(getattr(var, f"set_{key}")(sens))
-    
-    for i, key in enumerate(CELL_VOLTAGE_KEYS):
-        if key in config[CONF_SENSOR]:
-            sens = await sensor.new_sensor(config[CONF_SENSOR][key])
-            cg.add(var.set_cell_voltage_sensor(i, sens))
-    
-    for i, key in enumerate(TEMPERATURE_KEYS):
-        if key in config[CONF_SENSOR]:
-            sens = await sensor.new_sensor(config[CONF_SENSOR][key])
-            cg.add(var.set_temperature_sensor(i, sens))
+    if 'state_of_health_sensor' in config:
+        sens = yield sensor.new_sensor(config['state_of_health_sensor'])
+        cg.add(var.set_state_of_health_sensor(sens))
+
+    if 'state_of_charge_sensor' in config:
+        sens = yield sensor.new_sensor(config['state_of_charge_sensor'])
+        cg.add(var.set_state_of_charge_sensor(sens))
+
+    if 'cell_max_volt_diff_sensor' in config:
+        sens = yield sensor.new_sensor(config['cell_max_volt_diff_sensor'])
+        cg.add(var.set_cell_max_volt_diff_sensor(sens))
+
+    if 'charge_fet_sensor' in config:
+        sens = yield sensor.new_sensor(config['charge_fet_sensor'])
+        cg.add(var.set_charge_fet_sensor(sens))
+
+    if 'discharge_fet_sensor' in config:
+        sens = yield sensor.new_sensor(config['discharge_fet_sensor'])
+        cg.add(var.set_discharge_fet_sensor(sens))
+
+    if 'balancing1_sensor' in config:
+        sens = yield sensor.new_sensor(config['balancing1_sensor'])
+        cg.add(var.set_balancing1_sensor(sens))
+
+    if 'balancing2_sensor' in config:
+        sens = yield sensor.new_sensor(config['balancing2_sensor'])
+        cg.add(var.set_balancing2_sensor(sens))
+
+    if 'ac_in_sensor' in config:
+        sens = yield sensor.new_sensor(config['ac_in_sensor'])
+        cg.add(var.set_ac_in_sensor(sens))
+
+    if 'current_limit_sensor' in config:
+        sens = yield sensor.new_sensor(config['current_limit_sensor'])
+        cg.add(var.set_current_limit_sensor(sens))
+
+    if 'heart_sensor' in config:
+        sens = yield sensor.new_sensor(config['heart_sensor'])
+        cg.add(var.set_heart_sensor(sens))
+
+    if 'pack_indicate_sensor' in config:
+        sens = yield sensor.new_sensor(config['pack_indicate_sensor'])
+        cg.add(var.set_pack_indicate_sensor(sens))
+
+    if 'protection_discharge_current_sensor' in config:
+        sens = yield sensor.new_sensor(config['protection_discharge_current_sensor'])
+        cg.add(var.set_protection_discharge_current_sensor(sens))
+
+    if 'protection_charge_current_sensor' in config:
+        sens = yield sensor.new_sensor(config['protection_charge_current_sensor'])
+        cg.add(var.set_protection_charge_current_sensor(sens))
+
+    if 'protection_short_circuit_sensor' in config:
+        sens = yield sensor.new_sensor(config['protection_short_circuit_sensor'])
+        cg.add(var.set_protection_short_circuit_sensor(sens))
+
+    if 'reverse_sensor' in config:
+        sens = yield sensor.new_sensor(config['reverse_sensor'])
+        cg.add(var.set_reverse_sensor(sens))
+
+    # Nastavení senzorů pro napětí jednotlivých článků
+    for i in range(1, 18):
+        key = f'cell_voltage_sensor_{i}'
+        if key in config:
+            sens = yield sensor.new_sensor(config[key])
+            cg.add(var.set_cell_voltage_sensor(i - 1, sens))  # Indexované od 0
+
+    # Nastavení teplotních senzorů
+    for i in range(1, 7):
+        key = f'temperature_sensor_{i}'
+        if key in config:
+            sens = yield sensor.new_sensor(config[key])
+            cg.add(var.set_temperature_sensor(i - 1, sens))  # Indexované od 0
+
+    cg.add(var.init())
