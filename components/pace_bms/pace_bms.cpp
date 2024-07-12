1ca176e19aaa55fc0ee3edaf3187065c14dfea94
dansck <git@simecek.eu>
2024-07-12 21:02:51 +0200

    devel 2

--- components/pace_bms/pace_bms.cpp
+++ components/pace_bms/pace_bms.cpp
@@ -33,16 +33,15 @@ void PaceBMS::update() {
   publish_sensor_state(get_protection_charge_current(), this->protection_charge_current_sensor, "Protection Charge Current");
   publish_sensor_state(get_protection_short_circuit(), this->protection_short_circuit_sensor, "Protection Short Circuit");
   publish_sensor_state(get_reverse(), this->reverse_sensor, "Reverse");
-  publish_sensor_state(get_temperature(), this->temperature_sensor, "Temperature");
-  
+
   // Read cell voltages
   for (int i = 0; i < 16; i++) {
-    publish_sensor_state(get_cell_voltage(i), this->cell_voltages_sensors[i], "Cell Voltage", i);
+    publish_sensor_state(get_cell_voltage(i), this->cell_voltages_sensor[i], "Cell Voltage", i);
   }
 
   // Read temperatures
   for (int i = 0; i < 4; i++) {
-    publish_sensor_state(get_temperature(i), this->temperatures_sensors[i], "Temperature", i);
+    publish_sensor_state(get_temperature(i), this->temperatures_sensor[i], "Temperature", i);
   }
 
   publish_sensor_state(get_balancing_1(), this->balancing_1_sensor, "Balancing 1");
@@ -61,7 +60,7 @@ void PaceBMS::update() {
   publish_sensor_state(get_warn_info(), this->warn_info_sensor, "Warn Info");
 }
 
-void PaceBMS::publish_sensor_state(uint16_t value, sensor::Sensor *sensor, const char *sensor_name, int index = -1) {
+void PaceBMS::publish_sensor_state(uint16_t value, sensor::Sensor *sensor, const char *sensor_name, int index) {
   if (value != 0) {
     sensor->publish_state(value);
     if (index >= 0) {
@@ -69,18 +68,25 @@ void PaceBMS::publish_sensor_state(uint16_t value, sensor::Sensor *sensor, const
     } else {
       ESP_LOGI(TAG, "%s: %d", sensor_name, value);
     }
+  } else {
+    if (index >= 0) {
+      ESP_LOGW(TAG, "Failed to get %s [%d]", sensor_name, index);
+    } else {
+      ESP_LOGW(TAG, "Failed to get %s", sensor_name);
+    }
   }
 }
 
 uint16_t PaceBMS::crc16(const uint8_t *data, size_t length) {
   uint16_t crc = 0xFFFF;
-  for (size_t i = 0; i < length; i++) {
-    crc ^= data[i];
-    for (int j = 0; j < 8; j++) {
-      if (crc & 1)
+  while (length--) {
+    crc ^= *data++;
+    for (int i = 0; i < 8; i++) {
+      if (crc & 0x01) {
         crc = (crc >> 1) ^ 0xA001;
-      else
+      } else {
         crc >>= 1;
+      }
     }
   }
   return crc;
@@ -88,10 +94,10 @@ uint16_t PaceBMS::crc16(const uint8_t *data, size_t length) {
 
 uint8_t PaceBMS::lchksum_calc(const std::string &lenid) {
   uint8_t sum = 0;
-  for (char ch : lenid) {
-    sum += ch;
+  for (char c : lenid) {
+    sum += c;
   }
-  return ((sum & 0xFF) ^ 0xFF) + 1;
+  return (sum % 256);
 }
 
 uint8_t PaceBMS::chksum_calc(const std::vector<uint8_t> &data) {
@@ -99,12 +105,30 @@ uint8_t PaceBMS::chksum_calc(const std::vector<uint8_t> &data) {
   for (uint8_t byte : data) {
     sum += byte;
   }
-  return ((sum & 0xFF) ^ 0xFF) + 1;
+  return (sum % 256);
 }
 
-void PaceBMS::send_command(const uint8_t *command, size_t length) {
-  this->write_array(command, length);
-  ESP_LOGD(TAG, "Sent command: %s", format_hex_pretty(command, length).c_str());
+std::vector<uint8_t> PaceBMS::build_command(uint8_t cid1, uint8_t cid2, uint8_t idx) {
+  std::vector<uint8_t> command;
+  command.push_back(0x7E);  // Start byte
+  command.push_back(0x32);  // Version byte 1
+  command.push_back(0x35);  // Version byte 2
+  command.push_back(0x30);  // Address byte 1
+  command.push_back(0x31);  // Address byte 2
+  command.push_back(cid1);
+  command.push_back(cid2);
+  std::string lenid = "000";
+  command.push_back(lchksum_calc(lenid));
+  command.insert(command.end(), lenid.begin(), lenid.end());
+  command.push_back(chksum_calc(command));
+  command.push_back(0x0D);  // End byte
+  return command;
+}
+
+void PaceBMS::send_command(const std::vector<uint8_t> &command) {
+  for (uint8_t byte : command) {
+    this->write_byte(byte);
+  }
 }
 
 std::vector<uint8_t> PaceBMS::read_response() {
@@ -112,187 +136,162 @@ std::vector<uint8_t> PaceBMS::read_response() {
   while (this->available()) {
     response.push_back(this->read());
   }
-  ESP_LOGD(TAG, "Received response: %s", format_hex_pretty(response).c_str());
   return response;
 }
 
 uint16_t PaceBMS::decode_response(const std::vector<uint8_t> &response) {
-  if (response.size() < 2) {
-    ESP_LOGE(TAG, "Invalid response length");
-    return 0;
-  }
-
-  uint16_t received_crc = (response[response.size() - 2] << 8) | response[response.size() - 1];
-  uint16_t calculated_crc = crc16(response.data(), response.size() - 2);
-
-  if (received_crc != calculated_crc) {
-    ESP_LOGE(TAG, "CRC check failed");
-    return 0;
-  }
-
-  uint16_t value = (response[0] << 8) | response[1];
-  return value;
+  // Assume the response is correct and decode it.
+  // This function needs to be adjusted according to the actual response structure.
+  if (response.size() < 5) return 0;  // Example check for minimum length
+  return (response[3] << 8) | response[4];  // Example decoding
 }
 
-std::vector<uint8_t> PaceBMS::build_command(uint8_t cid1, uint8_t cid2, uint8_t idx = 0x00) {
-  std::vector<uint8_t> command = {0x7e, 0x32, 0x35, 0x30, 0x31, cid1, cid2, idx};
-  uint8_t l_checksum = lchksum_calc(std::string(command.begin(), command.end()));
-  command.push_back(l_checksum);
-  uint8_t checksum = chksum_calc(command);
-  command.push_back(checksum);
-  command.push_back(0x0D);
-  return command;
-}
-
-uint16_t PaceBMS::execute_command(uint8_t cid1, uint8_t cid2, uint8_t idx = 0x00) {
+uint16_t PaceBMS::execute_command(uint8_t cid1, uint8_t cid2, uint8_t idx) {
   std::vector<uint8_t> command = build_command(cid1, cid2, idx);
-  send_command(command.data(), command.size());
+  send_command(command);
   std::vector<uint8_t> response = read_response();
   return decode_response(response);
 }
 
+// Command methods implementation
 uint16_t PaceBMS::get_voltage() {
-  return execute_command(0x34, 0x43);
+  return execute_command(0x46, 0xC1);
 }
 
 uint16_t PaceBMS::get_current() {
-  return execute_command(0x34, 0x44);
+  return execute_command(0x46, 0xC2);
 }
 
 uint16_t PaceBMS::get_remaining_capacity() {
-  return execute_command(0x34, 0x45);
+  return execute_command(0x46, 0xC3);
 }
 
 uint16_t PaceBMS::get_nominal_capacity() {
-  return execute_command(0x34, 0x46);
+  return execute_command(0x46, 0xC4);
 }
 
 uint16_t PaceBMS::get_full_capacity() {
-  return execute_command(0x34, 0x47);
+  return execute_command(0x46, 0xC5);
 }
 
 uint16_t PaceBMS::get_cycles() {
-  return execute_command(0x34, 0x48);
+  return execute_command(0x46, 0xC6);
 }
 
 uint16_t PaceBMS::get_state_of_health() {
-  return execute_command(0x34, 0x49);
+  return execute_command(0x46, 0xC7);
 }
 
 uint16_t PaceBMS::get_state_of_charge() {
-  return execute_command(0x34, 0x4A);
+  return execute_command(0x46, 0xC8);
 }
 
 uint16_t PaceBMS::get_cell_max_volt_diff() {
-  return execute_command(0x34, 0x4B);
+  return execute_command(0x46, 0xC9);
 }
 
 uint16_t PaceBMS::get_charge_fet() {
-  return execute_command(0x34, 0x4C);
+  return execute_command(0x46, 0xCA);
 }
 
 uint16_t PaceBMS::get_discharge_fet() {
-  return execute_command(0x34, 0x4D);
+  return execute_command(0x46, 0xCB);
 }
 
 uint16_t PaceBMS::get_ac_in() {
-  return execute_command(0x34, 0x4E);
+  return execute_command(0x46, 0xCC);
 }
 
 uint16_t PaceBMS::get_current_limit() {
-  return execute_command(0x34, 0x4F);
+  return execute_command(0x46, 0xCD);
 }
 
 uint16_t PaceBMS::get_heart() {
-  return execute_command(0x34, 0x50);
+  return execute_command(0x46, 0xCE);
 }
 
 uint16_t PaceBMS::get_pack_indicate() {
-  return execute_command(0x34, 0x51);
+  return execute_command(0x46, 0xCF);
 }
 
 uint16_t PaceBMS::get_protection_discharge_current() {
-  return execute_command(0x34, 0x52);
+  return execute_command(0x46, 0xD0);
 }
 
 uint16_t PaceBMS::get_protection_charge_current() {
-  return execute_command(0x34, 0x53);
+  return execute_command(0x46, 0xD1);
 }
 
 uint16_t PaceBMS::get_protection_short_circuit() {
-  return execute_command(0x34, 0x54);
+  return execute_command(0x46, 0xD2);
 }
 
 uint16_t PaceBMS::get_reverse() {
-  return execute_command(0x34, 0x55);
+  return execute_command(0x46, 0xD3);
 }
 
 uint16_t PaceBMS::get_temperature(uint8_t idx) {
-  return execute_command(0x34, 0x56, idx);
+  return execute_command(0x46, 0xD4, idx);
 }
 
 uint16_t PaceBMS::get_cell_voltage(uint8_t idx) {
-  return execute_command(0x34, 0x57, idx);
-}
-
-uint16_t PaceBMS::get_temperatures(uint8_t idx) {
-  return execute_command(0x34, 0x58, idx);
+  return execute_command(0x46, 0xD5, idx);
 }
 
 uint16_t PaceBMS::get_balancing_1() {
-  return execute_command(0x34, 0x59);
+  return execute_command(0x46, 0xD6);
 }
 
 uint16_t PaceBMS::get_balancing_2() {
-  return execute_command(0x34, 0x5A);
+  return execute_command(0x46, 0xD7);
 }
 
 uint16_t PaceBMS::get_warnings() {
-  return execute_command(0x34, 0x5B);
+  return execute_command(0x46, 0xD8);
 }
 
 uint16_t PaceBMS::get_design_capacity() {
-  return execute_command(0x34, 0x5C);
+  return execute_command(0x46, 0xD9);
 }
 
 uint16_t PaceBMS::get_pack_full_capacity() {
-  return execute_command(0x34, 0x5D);
+  return execute_command(0x46, 0xDA);
 }
 
 uint16_t PaceBMS::get_pack_remaining_capacity() {
-  return execute_command(0x34, 0x5E);
+  return execute_command(0x46, 0xDB);
 }
 
 uint16_t PaceBMS::get_pack_state_of_health() {
-  return execute_command(0x34, 0x5F);
+  return execute_command(0x46, 0xDC);
 }
 
 uint16_t PaceBMS::get_pack_state_of_charge() {
-  return execute_command(0x34, 0x60);
+  return execute_command(0x46, 0xDD);
 }
 
 uint16_t PaceBMS::get_pack_number() {
-  return execute_command(0x34, 0x61);
+  return execute_command(0x46, 0xDE);
 }
 
 uint16_t PaceBMS::get_pack_analog_data() {
-  return execute_command(0x34, 0x62);
+  return execute_command(0x46, 0xDF);
 }
 
 uint16_t PaceBMS::get_software_version() {
-  return execute_command(0x34, 0x63);
+  return execute_command(0x46, 0xE0);
 }
 
 uint16_t PaceBMS::get_serial_number() {
-  return execute_command(0x34, 0x64);
+  return execute_command(0x46, 0xE1);
 }
 
 uint16_t PaceBMS::get_pack_capacity() {
-  return execute_command(0x34, 0x65);
+  return execute_command(0x46, 0xE2);
 }
 
 uint16_t PaceBMS::get_warn_info() {
-  return execute_command(0x34, 0x66);
+  return execute_command(0x46, 0xE3);
 }
 
 }  // namespace pace_bms
--- components/pace_bms/pace_bms.h
+++ components/pace_bms/pace_bms.h
@@ -1,68 +1,65 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
-#include <vector>
-#include <string>
 
 namespace esphome {
 namespace pace_bms {
 
-class PaceBMS : public PollingComponent, public uart::UARTDevice {
+class PaceBMS : public Component, public UARTDevice {
  public:
   void setup() override;
   void update() override;
 
-  // Declare all sensors
-  sensor::Sensor *voltage_sensor = new sensor::Sensor();
-  sensor::Sensor *current_sensor = new sensor::Sensor();
-  sensor::Sensor *remaining_capacity_sensor = new sensor::Sensor();
-  sensor::Sensor *nominal_capacity_sensor = new sensor::Sensor();
-  sensor::Sensor *full_capacity_sensor = new sensor::Sensor();
-  sensor::Sensor *cycles_sensor = new sensor::Sensor();
-  sensor::Sensor *state_of_health_sensor = new sensor::Sensor();
-  sensor::Sensor *state_of_charge_sensor = new sensor::Sensor();
-  sensor::Sensor *cell_max_volt_diff_sensor = new sensor::Sensor();
-  sensor::Sensor *charge_fet_sensor = new sensor::Sensor();
-  sensor::Sensor *discharge_fet_sensor = new sensor::Sensor();
-  sensor::Sensor *ac_in_sensor = new sensor::Sensor();
-  sensor::Sensor *current_limit_sensor = new sensor::Sensor();
-  sensor::Sensor *heart_sensor = new sensor::Sensor();
-  sensor::Sensor *pack_indicate_sensor = new sensor::Sensor();
-  sensor::Sensor *protection_discharge_current_sensor = new sensor::Sensor();
-  sensor::Sensor *protection_charge_current_sensor = new sensor::Sensor();
-  sensor::Sensor *protection_short_circuit_sensor = new sensor::Sensor();
-  sensor::Sensor *reverse_sensor = new sensor::Sensor();
-  sensor::Sensor *temperature_sensor = new sensor::Sensor();
-  sensor::Sensor *cell_voltages_sensor = new sensor::Sensor();
-  sensor::Sensor *temperatures_sensor = new sensor::Sensor();
-  sensor::Sensor *balancing_1_sensor = new sensor::Sensor();
-  sensor::Sensor *balancing_2_sensor = new sensor::Sensor();
-  sensor::Sensor *warnings_sensor = new sensor::Sensor();
-  sensor::Sensor *design_capacity_sensor = new sensor::Sensor();
-  sensor::Sensor *pack_full_capacity_sensor = new sensor::Sensor();
-  sensor::Sensor *pack_remaining_capacity_sensor = new sensor::Sensor();
-  sensor::Sensor *pack_state_of_health_sensor = new sensor::Sensor();
-  sensor::Sensor *pack_state_of_charge_sensor = new sensor::Sensor();
-  sensor::Sensor *pack_number_sensor = new sensor::Sensor();
-  sensor::Sensor *pack_analog_data_sensor = new sensor::Sensor();
-  sensor::Sensor *software_version_sensor = new sensor::Sensor();
-  sensor::Sensor *serial_number_sensor = new sensor::Sensor();
-  sensor::Sensor *pack_capacity_sensor = new sensor::Sensor();
-  sensor::Sensor *warn_info_sensor = new sensor::Sensor();
+  sensor::Sensor *voltage_sensor{nullptr};
+  sensor::Sensor *current_sensor{nullptr};
+  sensor::Sensor *remaining_capacity_sensor{nullptr};
+  sensor::Sensor *nominal_capacity_sensor{nullptr};
+  sensor::Sensor *full_capacity_sensor{nullptr};
+  sensor::Sensor *cycles_sensor{nullptr};
+  sensor::Sensor *state_of_health_sensor{nullptr};
+  sensor::Sensor *state_of_charge_sensor{nullptr};
+  sensor::Sensor *cell_max_volt_diff_sensor{nullptr};
+  sensor::Sensor *charge_fet_sensor{nullptr};
+  sensor::Sensor *discharge_fet_sensor{nullptr};
+  sensor::Sensor *ac_in_sensor{nullptr};
+  sensor::Sensor *current_limit_sensor{nullptr};
+  sensor::Sensor *heart_sensor{nullptr};
+  sensor::Sensor *pack_indicate_sensor{nullptr};
+  sensor::Sensor *protection_discharge_current_sensor{nullptr};
+  sensor::Sensor *protection_charge_current_sensor{nullptr};
+  sensor::Sensor *protection_short_circuit_sensor{nullptr};
+  sensor::Sensor *reverse_sensor{nullptr};
+  sensor::Sensor *temperature_sensor{nullptr};
+  std::array<sensor::Sensor *, 16> cell_voltages_sensor{};
+  std::array<sensor::Sensor *, 4> temperatures_sensor{};
+  sensor::Sensor *balancing_1_sensor{nullptr};
+  sensor::Sensor *balancing_2_sensor{nullptr};
+  sensor::Sensor *warnings_sensor{nullptr};
+  sensor::Sensor *design_capacity_sensor{nullptr};
+  sensor::Sensor *pack_full_capacity_sensor{nullptr};
+  sensor::Sensor *pack_remaining_capacity_sensor{nullptr};
+  sensor::Sensor *pack_state_of_health_sensor{nullptr};
+  sensor::Sensor *pack_state_of_charge_sensor{nullptr};
+  sensor::Sensor *pack_number_sensor{nullptr};
+  sensor::Sensor *pack_analog_data_sensor{nullptr};
+  sensor::Sensor *software_version_sensor{nullptr};
+  sensor::Sensor *serial_number_sensor{nullptr};
+  sensor::Sensor *pack_capacity_sensor{nullptr};
+  sensor::Sensor *warn_info_sensor{nullptr};
 
  protected:
-  std::pair<bool, std::vector<uint8_t>> bms_request(const uint8_t *ver, const uint8_t *adr, const uint8_t *cid1, const uint8_t *cid2, const std::vector<uint8_t> &info);
-  
+  void publish_sensor_state(uint16_t value, sensor::Sensor *sensor, const char *sensor_name, int index = -1);
   uint16_t crc16(const uint8_t *data, size_t length);
   uint8_t lchksum_calc(const std::string &lenid);
   uint8_t chksum_calc(const std::vector<uint8_t> &data);
-  void send_command(const uint8_t *command, size_t length);
+  void send_command(const std::vector<uint8_t> &command);
   std::vector<uint8_t> read_response();
   uint16_t decode_response(const std::vector<uint8_t> &response);
+  std::vector<uint8_t> build_command(uint8_t cid1, uint8_t cid2, uint8_t idx = 0x00);
+  uint16_t execute_command(uint8_t cid1, uint8_t cid2, uint8_t idx = 0x00);
 
-  // Methods to retrieve sensor data
+  // Command methods
   uint16_t get_voltage();
   uint16_t get_current();
   uint16_t get_remaining_capacity();
@@ -82,9 +79,8 @@ class PaceBMS : public PollingComponent, public uart::UARTDevice {
   uint16_t get_protection_charge_current();
   uint16_t get_protection_short_circuit();
   uint16_t get_reverse();
-  uint16_t get_temperature();
-  uint16_t get_cell_voltages();
-  uint16_t get_temperatures();
+  uint16_t get_temperature(uint8_t idx);
+  uint16_t get_cell_voltage(uint8_t idx);
   uint16_t get_balancing_1();
   uint16_t get_balancing_2();
   uint16_t get_warnings();
